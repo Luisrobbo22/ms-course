3d97a3ba3cf3dbe138e3c1ea3234017357552679
Luis Felipe Robbo <luis_robbo@hotmail.com>
2022-03-16 19:52:29 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/br/com/robbo/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robbo.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
