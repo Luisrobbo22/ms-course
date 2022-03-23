c378a0fdd18f8e4ea2f027a8c1daa53a045a7a59
Luis Felipe Robbo <luis_robbo@hotmail.com>
2022-03-22 21:52:28 -0300

    hr-user postgres dev profile

--- hr-worker/src/main/java/br/com/robbo/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/br/com/robbo/hrworker/repositories/WorkerRepository.java
@@ -2,6 +2,8 @@ package br.com.robbo.hrworker.repositories;
 
 import br.com.robbo.hrworker.entities.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 }
